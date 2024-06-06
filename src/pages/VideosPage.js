2dd1790dfd9459e19bb73c22461b0ba125267c69
Stuart Fleisher <stufleisher@gmail.com>
2024-06-06 12:00:27 -0400

    added filtering by role

--- src/pages/VideosPage.js
+++ src/pages/VideosPage.js
@@ -1,13 +1,17 @@
 import videos from "../data/videos";
 import VideoCard from "../components/VideoCard";
 import "./VideosPage.css";
-import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
-export default function VideosPage({useBorder}) {
+export default function VideosPage({ useBorder }) {
   const [searchParams] = useSearchParams();
   const role = searchParams.get("role");
+  const [selected, setSelected] = useState(role);
 
-  const selected = role
+  const navigate = useNavigate();
+
+  const activeVideos = role
     ?
     videos.filter((video) => video.roles.includes(role))
     :
@@ -17,7 +21,27 @@ export default function VideosPage({useBorder}) {
     <div
       className={`VideosPage`}
     >
-      {selected.map((video) => (
+      <div className="filterVideoForm border-solid background-accent2 texture-noise1">
+
+        <form>
+          <label htmlFor="filter">Filter by Role:</label>
+          <select
+            name="filter"
+            value={selected}
+            onChange={(e) => {
+              const targetUrl = e.target.value ? `/videos?role=${e.target.value}` : '/videos';
+              setSelected(e.target.value);
+              navigate(targetUrl);
+            }}
+          >
+            <option value={""}> Any Role</option>
+            <option value={'Animator'}> Animator </option>
+            <option value={'Director'}> Director</option>
+            <option value={'Copywriter'}> Copywriter </option>
+          </select>
+        </form>
+      </div>
+      {activeVideos.map((video) => (
         <VideoCard video={video} key={video.id} />
       ))}
     </div>
